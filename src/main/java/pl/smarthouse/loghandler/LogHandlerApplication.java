89b0f1dbad5159f1895b3590e22fe12c21f64f26
majkelp1982 <s.pomazanka@gmail.com>
2021-11-07 16:36:15 +0100

    add LogConfiguration

--- src/main/java/pl/smarthouse/loghandler/LogHandlerApplication.java
+++ src/main/java/pl/smarthouse/loghandler/LogHandlerApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ErrorHandlerApplication {
+public class LogHandlerApplication {
 
-	public static void main(final String[] args) {
-		SpringApplication.run(ErrorHandlerApplication.class, args);
-	}
-
+  public static void main(final String[] args) {
+    SpringApplication.run(LogHandlerApplication.class, args);
+  }
 }
